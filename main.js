c42edaf315fc9dd647310be7ebd5a6d262fbd70d
Rsenju <msena.rebeca@gmail.com>
2025-03-29 18:18:54 -0300

    Ajuste de sincronização inicial

---
+++ main.js
@@ -0,0 +1,25 @@
+function togglePesquisa() {
+    document.querySelector(".lupa").classList.toggle("active");
+}
+
+function verificarAdmin() {
+    let senha = prompt("Digite a senha do administrador:");
+    if (senha === "123") { 
+        document.getElementById("status-opcoes").style.display = "block";
+    } else {
+        alert("Acesso negado!");
+    }
+}
+
+function atualizarStatus() {
+    let status = document.getElementById("status-select").value;
+    let statusText = document.getElementById("status-text");
+
+    if (status === "adotado") {
+        statusText.textContent = "Adotado";
+        statusText.className = "status-adotado";
+    } else {
+        statusText.textContent = "Disponível";
+        statusText.className = "status-disponivel";
+    }
+}
